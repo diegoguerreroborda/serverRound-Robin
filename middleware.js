0299507ad87697c66b7e8606e11b922f61a95313
diegoguerreroborda <diego.guerrero02@uptc.edu.co>
2020-12-13 17:30:26 -0500

    prim

--- middleware.js
+++ middleware.js
@@ -1,8 +1,12 @@
 const express = require('express')
+
+
 const app = express()
-const cors = require('cors')
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('Servidor encendido')
+  })  
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)

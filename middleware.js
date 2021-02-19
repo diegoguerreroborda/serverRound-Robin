72c5c861b2db7ffdb297f266bbd75f0e1e974623
diegoguerreroborda <diego.guerrero02@uptc.edu.co>
2021-02-19 16:17:34 -0500

    Estado de los servidores agregado

--- middleware.js
+++ middleware.js
@@ -8,11 +8,11 @@ const app = express()
 app.use(cors())
 
 let count = 0;
-let hosts = ["http://localhost:3000/", "http://localhost:3001/", "http://localhost:3002/"];
+let hosts = [{path: "http://localhost:3000/", alive: false}, {path: "http://localhost:3001/", alive: false}, {path: "http://localhost:3002/", alive: false}];
 let urlG;
 
-var checkCount = (req, res, next) => {
-    if(count <= hosts.length-1){
+var checkCount = () => {
+    if(count < hosts.length-1){
         count++;
     }else{
         count = 0;
@@ -22,10 +22,11 @@ var checkCount = (req, res, next) => {
 app.all('/send_image', async(req, res, next) => {
     let pass = true
     while (pass) {
-        console.log(hosts[count]);
-        urlG = hosts[count];
-        checkCount();
+        console.log('Hola?', hosts[count].path)
+        urlG = hosts[count].path
+        checkCount()
         try {
+            console.log('try')
             res = await axios(urlG)
             pass = false;
         } catch(err) {
@@ -64,6 +65,24 @@ app.get('/received_image', (req, res) => {
         console.log("Error al recibir imagen");
     });
 });
+
+app.get('/info_servers', async(req,res) => {
+    //Envía json al cliente del estado de los servers
+    for (const host in hosts) {
+        try {
+            response = await axios(hosts[host].path)
+            hosts[host].alive = true;
+        } catch(err) {
+            hosts[host].alive = false;
+            console.log(err.toString())
+        }
+    }
+    res.json(hosts);
+  })
+  
+  app.get('/create_server', (req, res) => {
+      //Crear linea en el bash para crear una instancia de docker y agregarla a la lista
+  })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
